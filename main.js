b14f83dd9c44f021b3e258f895fd69d5ca7b05c8
Lennart Landsmeer <lennart@landsmeer.email>
2017-05-12 20:22:56 +0200

    monotonic clock class

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-var canvas = document.getElementById('mainCanvas');
-var ctx = canvas.getContext('2d');
+const canvas = document.getElementById('mainCanvas');
+const ctx = canvas.getContext('2d');
 ctx.moveTo(0, 0);
 ctx.lineTo(200, 200);
 ctx.stroke();
+
+class Monotonic {
+    constructor() {
+        this.offset = 0;
+        this.seen = 0;
+    }
+
+    time() {
+        const now = Date.now();
+        if (now < this.seen) {
+            this.offset += this.seen - now;
+        }
+        this.seen = now;
+        return now + this.offset;
+    }
+}
+
+const clock = new Monotonic();
+
+function loop() {
+    setTimeout(loop)
+}
