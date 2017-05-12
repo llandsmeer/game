0724361dab0450941dc240cd5bcd386b8af5e8e7
Lennart Landsmeer <lennart@landsmeer.email>
2017-05-12 22:44:49 +0200

    bullet

--- bullet.js
+++ bullet.js
@@ -1,13 +1,23 @@
 class Bullet {
-    constructor() {
-        console.log ('hai')
+    constructor(x, y, angle, speed) {
+        angle = angle || 0;
+        speed = speed || 40;
+        this.x = x || 0;
+        this.y = y || 0;
+        this.dx = speed * Math.cos(angle);
+        this.dy = speed * Math.sin(angle);
+        this.angle = angle;
     }
 
     update(gameState) {
-
+        this.x += this.dx * gameState.dt;
+        this.y += this.dy * gameState.dt;
     }
 
     draw(context) {
-
+        context.beginPath();
+        context.fillStyle = '#000';
+        context.fillRect(this.x-5, this.y-5, 10, 10);
+        context.closePath();
     }
 }
